863723c9f25eaf6a94169100bf538214517c97ce
Britt Crawford <britt.v.crawford@gmail.com>
2017-05-09 08:35:57 -0700

    It helps to implement the spec correctly

--- graph.go
+++ graph.go
@@ -7,26 +7,24 @@ type Metadata map[string]string
 
 // Graph is a set of nodes and edges. Graph is a json.Marshaler and a json.Unmarshaler.
 type Graph struct {
-	Label     string   `json:"label,omitempty"`
-	GraphType string   `json:"graph_type,omitempty"`
-	Directed  bool     `json:"directed"`
-	Metadata  Metadata `json:"metadata,omitempty"`
-	Nodes     []Node   `json:"nodes"`
-	Edges     []Edge   `json:"edges"`
+	Label    string   `json:"label,omitempty"`
+	Directed bool     `json:"directed"`
+	Metadata Metadata `json:"metadata,omitempty"`
+	Nodes    []Node   `json:"nodes"`
+	Edges    []Edge   `json:"edges"`
 }
 
 // Node is a point on a graph. Node is a json.Marshaler and a json.Unmarshaler.
 type Node struct {
 	ID       string   `json:"id"`
 	Label    string   `json:"label,omitempty"`
-	NodeType string   `json:"node_type,omitempty"`
 	Metadata Metadata `json:"metadata,omitempty"`
 }
 
 // Edge is a connection between two nodes. Edge is a json.Marshaler and a json.Unmarshaler.
 type Edge struct {
-	SourceID string   `json:"source_id"`
-	TargetID string   `json:"target_id"`
+	SourceID string   `json:"source"`
+	TargetID string   `json:"target"`
 	Directed bool     `json:"directed"`
 	Relation string   `json:"relation,omitempty"`
 	Metadata Metadata `json:"metadata,omitempty"`
--- graph_test.go
+++ graph_test.go
@@ -34,7 +34,7 @@ func TestMarshalling(t *testing.T) {
 		t.Fail()
 	}
 
-	expected := `{"label":"Test Graph","directed":false,"nodes":[{"id":"Node #1"},{"id":"Node #2"},{"id":"Node #3"},{"id":"Node #4"}],"edges":[{"source_id":"Node #1","target_id":"Node #2","directed":false},{"source_id":"Node #2","target_id":"Node #3","directed":false},{"source_id":"Node #2","target_id":"Node #4","directed":false}]}`
+	expected := `{"label":"Test Graph","directed":false,"nodes":[{"id":"Node #1"},{"id":"Node #2"},{"id":"Node #3"},{"id":"Node #4"}],"edges":[{"source":"Node #1","target":"Node #2","directed":false},{"source":"Node #2","target":"Node #3","directed":false},{"source":"Node #2","target":"Node #4","directed":false}]}`
 	if string(b) != expected {
 		t.Logf("JSON was not what was expected.\n\nExpected: %s\nActual: %s", expected, string(b))
 		t.Fail()
